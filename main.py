9b59ce7c731ff8ce0617dd4afd8120e8bedaf112
Artuom4ik <masterskayaigr123@gmail.com>
2022-12-24 20:06:30 +0300

    Поправил код по PEP8

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import os 
-import requests 
-from pathlib import Path
+import os
+import requests
+import os.path
+
 from pprint import pprint
 from dotenv import load_dotenv
 from urllib.parse import urlparse
-import os.path
 
 
 def save_image(link, path):
@@ -19,7 +19,7 @@ def fetch_spacex_launch(id_launch):
     response = requests.get(url)
     links = response.json()["links"]["flickr"]["original"]
     for number, link in enumerate(links):
-        path = f"images/spacex_{number}.jpeg"    
+        path = f"images/spacex_{number}.jpeg"
         save_image(link, path)
 
 
@@ -30,16 +30,15 @@ def extensions(links):
 
 
 def nasa(api_nasa):
-    url = f"https://api.nasa.gov/planetary/apod?api_key={api_nasa}&count=5"
+    url = f"https://api.nasa.gov/planetary/apod?api_key={api_nasa}&count=30"
     response = requests.get(url)
     links = response.json()
     lst = []
     for link_1 in links:
         lst.append(link_1["hdurl"])
     for number, link in enumerate(lst):
-        path = f"images/nasa_apod_{number}.jpeg"    
+        path = f"images/nasa_apod_{number}.jpeg"
         save_image(link, path)
-
 
 
 load_dotenv()
@@ -48,7 +47,3 @@ os.makedirs("images", exist_ok=True)
 id_launch = "5eb87d46ffd86e000604b388"
 fetch_spacex_launch(id_launch)
 nasa(api_nasa)
-
-
-
-
