6367481f011b85237f60b9d860ffa73384afe6f4
Artuom4ik <masterskayaigr123@gmail.com>
2022-12-28 13:02:08 +0300

    Сделал изменения в fetch_spacex._launch.py

--- fetch_spacex_launch.py
+++ fetch_spacex_launch.py
@@ -1,7 +1,32 @@
+import os
+import argparse
+from save_image import save_image
+
+import requests
+from dotenv import load_dotenv
+
+
 def fetch_spacex_launch(id_launch):
     url = f"https://api.spacexdata.com/v5/launches/{id_launch}"
     response = requests.get(url)
     links = response.json()["links"]["flickr"]["original"]
     for number, link in enumerate(links):
-        path = f"images/spacex_{number}.jpeg"
+        path = os.path.join("images", f"spacex_{number}.jpeg")
         save_image(link, path)
+
+
+def get_id():
+    parser = argparse.ArgumentParser(
+        description="Скачивает фотографии SpaceX запуска ракет, по указанному id запуска."
+    )
+    load_dotenv()
+    id_launch = os.getenv("ID_LAUNCH")
+    parser.add_argument("id_launch", default=id_launch, type=str, nargs="?")
+    args = parser.parse_args()
+    return args.id_launch
+
+
+if __name__ == "__main__":
+    id_launch = get_id()
+    fetch_spacex_launch(id_launch)
+   
