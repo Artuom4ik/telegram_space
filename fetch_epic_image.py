efa26651963232bf225515691cf426527d5cc0f1
Artuom4ik <masterskayaigr123@gmail.com>
2022-12-29 19:01:33 +0300

    Оформил fetch_epic_image.py по PEP8

--- fetch_epic_image.py
+++ fetch_epic_image.py
@@ -1,3 +1,12 @@
+import os
+import datetime
+
+import requests
+from dotenv import load_dotenv
+
+from save_image import save_image
+
+
 def fetch_epic_image(nasa_token):
     params = {
         "api_key": nasa_token,
@@ -9,8 +18,16 @@ def fetch_epic_image(nasa_token):
     for number, image in enumerate(images_response):
         date = datetime.datetime.fromisoformat(image["date"])
         name_image = image["image"]
-        url_image = f"https://api.nasa.gov/EPIC/archive/natural/{now.year}/{now.month}/{now.day}/png/{name_image}.png"
+        url_image = f"""
+        https://api.nasa.gov/EPIC/archive/natural/{date.year}/{date.month}/{date.day}/png/{name_image}.png
+        """
         response_image = requests.get(url_image, params=params)
         response_image.raise_for_status()
         path = f"images/epic_image_{number}.png"
         save_image(response_image.url, path)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    nasa_token = os.getenv("API_NASA")
+    fetch_epic_image(nasa_token)
