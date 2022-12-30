af5aa4395b43816a8a4b34072c4ceac2426781d5
Artuom4ik <masterskayaigr123@gmail.com>
2022-12-30 13:44:57 +0300

    создал ptbot.py

---
+++ ptbot.py
@@ -0,0 +1,19 @@
+import os
+
+import telegram
+from dotenv import load_dotenv
+
+
+def telegram_bot():
+    load_dotenv()
+    chat_id = os.getenv("CHAT_ID")
+    api_bot = os.getenv("API_BOT")
+    bot = telegram.Bot(token=api_bot)
+    bot.send_document(
+        chat_id=chat_id,
+        document=open('images/nasa_apod_10.jpg', 'rb')
+    )
+
+
+if __name__ == "__main__":
+    telegram_bot()
