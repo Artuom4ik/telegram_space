a5a6b8de0893cc0c878e0991d05a8c39c51afd10
Artuom4ik <masterskayaigr123@gmail.com>
2023-01-08 12:14:06 +0300

    создал send_image.py

---
+++ send_image.py
@@ -0,0 +1,24 @@
+import os
+import random
+
+import telegram 
+from dotenv import load_dotenv
+
+
+
+def send_image(name_image, chat_id):
+    with open(f"images/{name_image}", "rb") as image:
+        bot.send_document(chat_id=chat_id, document=image)
+
+def random_image():
+    dirs = os.listdir("images")
+    return random.choice(dirs)
+
+if __name__ == "__main__":
+    load_dotenv()
+    api_bot = os.getenv("API_BOT")
+    chat_id = os.getenv("CHAT_ID")
+    name_image = random_image() 
+    bot = telegram.Bot(token=api_bot)
+    send_image(name_image, chat_id)
+   
