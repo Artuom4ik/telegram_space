d53675090058d59f5acde386b63fca552eda4e16
Artuom4ik <masterskayaigr123@gmail.com>
2023-01-13 18:58:33 +0300

    Добавил функцию get_time_sleep.

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,17 +1,46 @@
 import os
+import random
+import time
+import argparse
 
 import telegram
 from dotenv import load_dotenv
 
+from send_image import send_image, random_image 
+
 
 def telegram_bot(chat_id, api_bot):
     bot = telegram.Bot(token=api_bot)
-    
     bot.send_document(chat_id=chat_id, document=image)
+
+
+def send_random_images(chat_id, bot, time_sleep):
+    while True:
+        images = os.listdir("images")
+        random.shuffle(images)
+        for image in images:
+            send_image(image, chat_id, bot)
+            time.sleep(time_sleep)
+
+
+def get_time_sleep():
+    parser = argparse.ArgumentParser(
+        description="""
+        Отправляет фотографии по заданному вермени.
+        """
+    )
+    load_dotenv()
+    time_sleep = os.getenv("TIME_SLEEP")
+    parser.add_argument("time_sleep", default=time_sleep, type=str, nargs="?")
+    args = parser.parse_args()
+    return int(args.time_sleep)
 
 
 if __name__ == "__main__":
     load_dotenv()
     chat_id = os.getenv("CHAT_ID")
     api_bot = os.getenv("API_BOT")
-    telegram_bot(chat_id, api_bot)
+    bot = telegram.Bot(token=api_bot)
+    time_sleep = get_time_sleep()
+    send_random_images(chat_id, bot, time_sleep)
+
