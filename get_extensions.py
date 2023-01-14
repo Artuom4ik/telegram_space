7adecc16cac2191517ac50be41e16af8eb05a49d
Artuom4ik <masterskayaigr123@gmail.com>
2023-01-14 18:01:09 +0300

    Соединил скрипты в один скрипт.

--- get_extensions.py
+++
@@ -1,9 +0,0 @@
-import os.path
-
-from urllib.parse import urlparse
-
-
-def get_extensions(link):
-    path_image = urlparse(link).path
-    extensions_image = os.path.splitext(path_image)[1]
-    return extensions_image
--- save_image.py
+++ save_image.py
@@ -1,4 +1,8 @@
+import os.path
+
 import requests
+
+from urllib.parse import urlparse
 
 
 def save_image(link, path):
@@ -6,3 +10,9 @@ def save_image(link, path):
     response.raise_for_status()
     with open(path, "wb") as image:
         image.write(response.content)
+
+
+def get_extensions(link):
+    path_image = urlparse(link).path
+    extensions_image = os.path.splitext(path_image)[1]
+    return extensions_image
