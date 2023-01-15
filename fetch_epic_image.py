7e2db3256e76c3cb9a93f18ceacd9fda3a7c5705
Artuom4ik <masterskayaigr123@gmail.com>
2023-01-15 12:16:24 +0300

    Сделал изменения в  функции.

--- fetch_epic_image.py
+++ fetch_epic_image.py
@@ -1,33 +1,42 @@
 import os
 import datetime
+import argparse
 
 import requests
 from dotenv import load_dotenv
 
-from save_image import save_image
+from save_image import save_image, NAME_FOLDER, create_folder
 
 
-def fetch_epic_image(nasa_token):
+def fetch_epic_image(nasa_token, count):
     params = {
-        "api_key": nasa_token,
+        "api_key": nasa_token
     }
     url = "https://api.nasa.gov/EPIC/api/natural/images"
     response = requests.get(url, params=params)
     response.raise_for_status()
-    images_response = response.json()
+    images_response = response.json()[:count]
     for number, image in enumerate(images_response):
         date = datetime.datetime.fromisoformat(image["date"])
+        image_date = date.strftime("%Y/%m/%d")
         name_image = image["image"]
-        url_image = f"""
-        https://api.nasa.gov/EPIC/archive/natural/{date.year}/{date.month}/{date.day}/png/{name_image}.png
-        """
+        url_image = f"https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{name_image}.png"
         response_image = requests.get(url_image, params=params)
         response_image.raise_for_status()
-        path = os.path.join("images", f"epic_image_{number}.png")
+        path = os.path.join(NAME_FOLDER, f"epic_image_{number}.png")
         save_image(response_image.url, path)
 
 
 if __name__ == "__main__":
     load_dotenv()
     nasa_token = os.getenv("API_NASA")
-    fetch_epic_image(nasa_token)
+    parser = argparse.ArgumentParser(
+        description="""
+        Скачивает фотографии Земли.
+        """
+    )
+    parser.add_argument("count", default=10, type=int, nargs="?")
+    args = parser.parse_args()
+    count = args.count
+    create_folder(NAME_FOLDER)
+    fetch_epic_image(nasa_token, count)
