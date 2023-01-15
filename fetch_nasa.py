3150d27a482767f871360300c4ec9ac4c7b2326b
Artuom4ik <masterskayaigr123@gmail.com>
2023-01-15 12:20:55 +0300

    Сделал изменения в fetch_nasa.

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -4,8 +4,8 @@ import requests
 from dotenv import load_dotenv
 from pathlib import Path
 
-from save_image import save_image, get_extensions
-
+from save_image import save_image, get_extensions, NAME_FOLDER
+from save_image import create_folder
 
 
 def fetch_nasa(nasa_token):
@@ -13,7 +13,7 @@ def fetch_nasa(nasa_token):
         "api_key": nasa_token,
         "count": 30
     }
-    url = "https://api.nasa.gov/planetary/apod"    
+    url = "https://api.nasa.gov/planetary/apod"
     response = requests.get(url, params=params)
     response.raise_for_status()
     images_response = response.json()
@@ -21,11 +21,12 @@ def fetch_nasa(nasa_token):
         link_image = image["url"]
         extension = get_extensions(link_image)
         if image["media_type"] == "image":
-            path = os.path.join("images", f"nasa_apod_{number}{extension}")
+            path = os.path.join(NAME_FOLDER, f"nasa_apod_{number}{extension}")
             save_image(link_image, path)
 
 
 if __name__ == "__main__":
     load_dotenv()
+    create_folder(NAME_FOLDER)
     nasa_token = os.getenv("API_NASA")
     fetch_nasa(nasa_token)
