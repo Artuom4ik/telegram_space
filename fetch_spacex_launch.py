49b153df18d5dae128f8b65b765f85e5f0193b04
Artuom4ik <masterskayaigr123@gmail.com>
2023-01-15 12:19:45 +0300

    Дописал if main().

--- fetch_spacex_launch.py
+++ fetch_spacex_launch.py
@@ -4,7 +4,7 @@ import argparse
 import requests
 from dotenv import load_dotenv
 
-from save_image import save_image
+from save_image import save_image, create_folder, NAME_FOLDER
 
 
 def fetch_spacex_launch(id_launch):
@@ -12,23 +12,23 @@ def fetch_spacex_launch(id_launch):
     response = requests.get(url)
     links = response.json()["links"]["flickr"]["original"]
     for number, link in enumerate(links):
-        path = os.path.join("images", f"spacex_{number}.jpeg")
+        path = os.path.join(NAME_FOLDER, f"spacex_{number}.jpeg")
         save_image(link, path)
 
 
-def get_id():
+def get_id(id_launch):
     parser = argparse.ArgumentParser(
         description="""
         Скачивает фотографии SpaceX запуска ракет, по указанному id запуска.
         """
     )
-    load_dotenv()
-    id_launch = os.getenv("ID_LAUNCH")
     parser.add_argument("id_launch", default=id_launch, type=str, nargs="?")
     args = parser.parse_args()
     return args.id_launch
 
 
 if __name__ == "__main__":
-    id_launch = get_id()
-    fetch_spacex_launch(id_launch)
+    load_dotenv()
+    create_folder(NAME_FOLDER)
+    id_launch = os.getenv("ID_LAUNCH")
+    fetch_spacex_launch(get_id(id_launch))
