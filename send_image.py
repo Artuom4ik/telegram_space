8b4213f2bcd63ba149acaea77c20544c7d277ac3
Artuom4ik <masterskayaigr123@gmail.com>
2023-01-15 12:27:33 +0300

    Изменил название переменных.

--- send_image.py
+++ send_image.py
@@ -4,15 +4,17 @@ import random
 import telegram 
 from dotenv import load_dotenv
 
+from save_image import NAME_FOLDER
+
 
 
 def send_image(name_image, chat_id, bot):
-    with open(os.path.join("images", f"{name_image}"), "rb") as image:
+    with open(os.path.join(NAME_FOLDER, f"{name_image}"), "rb") as image:
         bot.send_document(chat_id=chat_id, document=image)
 
 def random_image():
-    dirs = os.listdir("images")
-    return random.choice(dirs)
+    images = os.listdir(NAME_FOLDER)
+    return random.choice(images)
 
 if __name__ == "__main__":
     load_dotenv()
