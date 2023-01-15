1173f07e1680721e15b5327430229af708b8e903
Artuom4ik <masterskayaigr123@gmail.com>
2023-01-15 13:01:43 +0300

    Добавил функцию get_name_file.

--- send_image.py
+++ send_image.py
@@ -1,26 +1,41 @@
 import os
 import random
+import argparse
 
-import telegram 
+import telegram
 from dotenv import load_dotenv
 
 from save_image import NAME_FOLDER
-
 
 
 def send_image(name_image, chat_id, bot):
     with open(os.path.join(NAME_FOLDER, f"{name_image}"), "rb") as image:
         bot.send_document(chat_id=chat_id, document=image)
 
-def random_image():
+
+def random_image(image_name):
+    if image_name:
+        return image_name    
     images = os.listdir(NAME_FOLDER)
     return random.choice(images)
+
+
+def get_name_file():
+    parser = argparse.ArgumentParser(
+        description="""
+        Отправляет фотографию из папки с изображениями.
+        """
+    )
+    parser.add_argument("-n", "--name_image")
+    args = parser.parse_args()
+    return args.name_image
+
 
 if __name__ == "__main__":
     load_dotenv()
     api_bot = os.getenv("API_BOT")
     chat_id = os.getenv("CHAT_ID")
-    name_image = random_image() 
     bot = telegram.Bot(token=api_bot)
-    send_image(name_image, chat_id)
+    name_image = random_image(get_name_file())
+    send_image(name_image, chat_id, bot)
    
