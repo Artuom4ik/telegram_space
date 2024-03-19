ff1c2bc50a7bac4e245d2cfae330b60e79ade4c1
Artuom4ik <masterskayaigr123@gmail.com>
2024-03-19 20:04:29 +0300

    Add conditions

--- fetch_spacex_launch.py
+++ fetch_spacex_launch.py
@@ -11,18 +11,23 @@ def fetch_spacex_launch(id_launch):
     url = f"https://api.spacexdata.com/v5/launches/{id_launch}"
     response = requests.get(url)
     links = response.json()["links"]["flickr"]["original"]
-    for number, link in enumerate(links):
-        path = os.path.join(NAME_FOLDER, f"spacex_{number}.jpeg")
-        save_image(link, path)
+
+    if links:
+        for number, link in enumerate(links):
+            path = os.path.join(NAME_FOLDER, f"spacex_{number}.jpeg")
+            save_image(link, path)
+    
+    else:
+        print('Изображения не найдены')
 
 
-def get_id(id_launch):
+def get_id():
     parser = argparse.ArgumentParser(
         description="""
         Скачивает фотографии SpaceX запуска ракет, по указанному id запуска.
         """
     )
-    parser.add_argument("id_launch", default=id_launch, type=str, nargs="?")
+    parser.add_argument("id_launch", type=str, nargs="?")
     args = parser.parse_args()
     return args.id_launch
 
@@ -30,5 +35,4 @@ def get_id(id_launch):
 if __name__ == "__main__":
     load_dotenv()
     create_folder(NAME_FOLDER)
-    id_launch = os.getenv("ID_LAUNCH")
-    fetch_spacex_launch(get_id(id_launch))
+    fetch_spacex_launch(get_id())
